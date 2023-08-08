4aefe0f310cbafb49d26c114995e21c1e80fc05b
Juan Echavarria <code.jfe@gmail.com>
2023-08-07 20:18:54 -0500

    first setup with expo

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
+import Button from '@/components/Button';
+import theme, { Text } from '@/utils/theme';
+import { ThemeProvider } from '@shopify/restyle';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text variant='text3Xl'>La App se cargó ful, pedofilo</Text>
+        <Button />
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { View} from 'react-native'
+import React from 'react'
+import { Box, Text } from '@/utils/theme'
+
+const Button = () => {
+    return (
+    <Box bg='primary' p='4' borderRadius='rounded-3xl'>
+        <Text color='white'>Componente</Text>
+    </Box>
+    )
+}
+
+export default Button
